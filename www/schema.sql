06e36bb740d62ab9e66a93ac00a7842c9752948f
yeh-George <ycj012@163.com>
2019-12-19 10:49:31 +0800

    day03 Models and create Mysql tables.

---
+++ www/schema.sql
@@ -0,0 +1,52 @@
+drop database if exists awesome;
+
+create database awesome;
+
+use awesome;
+
+create table users(
+    `id` varchar(50) not null,
+    `email` varchar(50) not null,
+    `passwd` varchar(50) not null,
+    `admin` bool not null,
+    `name` varchar(50) not null,
+    `image` varchar(500),
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    unique key `idx_email` (`email`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table blogs (
+    `id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500),
+    `name` varchar(50) not null,
+    `summary` varchar(200) not null,
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+    
+create table comments (
+    `id` varchar(50) not null,
+    `blog_id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `user_name` varchar(50) not null,
+    `user_image` varchar(500),
+    `content` mediumtext not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+
+
+
+
+
+
+
+
